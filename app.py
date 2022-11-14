f10c374dfe0b7eeb113de7694ac13d88872b41bf
Lgvalia <le.gvalia@gmail.com>
2022-11-14 19:31:24 +0400

    Commit 3

--- app.py
+++ app.py
@@ -2,49 +2,16 @@ import streamlit as st
 import plotly.express as px
 import pandas as pd
 
-st.write('# Which Social Platform Do Users click first')
-
-# -- Get the user input
-year_col, continent_col, log_x_col = st.columns([5, 5, 5])
-with year_col:
-    year_choice = st.slider(
-        "What year would you like to examine?",
-        min_value=1952,
-        max_value=2007,
-        step=5,
-        value=2007,
-    )
-with continent_col:
-    continent_choice = st.selectbox(
-        "What continent would you like to look at?",
-        ("All", "Asia", "Europe", "Africa", "Americas", "Oceania"),
-    )
-with log_x_col:
-    log_x_choice = st.checkbox("Log X Axis?")
-
-
+st.write('# Which Platform Users Click First')
 
 # -- Read in the data
-df = px.data.gapminder()
-# -- Apply the year filter given by the user
-filtered_df = df[(df.year == year_choice)]
-# -- Apply the continent filter
-if continent_choice != "All":
-    filtered_df = filtered_df[filtered_df.continent == continent_choice]
-
+df = pd.read_csv('WhatsgoodlyData-10.csv')
 
 
 # -- Create the figure in Plotly
-fig = px.scatter(
-    filtered_df,
-    x="gdpPercap",
-    y="lifeExp",
-    size="pop",
-    color="continent",
-    hover_name="country",
-    log_x=log_x_choice,
-    size_max=60,
-)
-fig.update_layout(title="GDP per Capita vs. Life Expectancy")
+data = df.groupby(by=['Segment Type','Answer'])['Count'].sum().reset_index()
+fig = px.bar(data, x='Segment Type',y='Count', color='Answer', text_auto=True, barmode='stack')
+fig.show()
+fig.update_layout(title="Platform Count by Segment Types")
 # -- Input the Plotly chart to the Streamlit interface
 st.plotly_chart(fig, use_container_width=True)
