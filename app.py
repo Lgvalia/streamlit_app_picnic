37d8b8a21a7c37a9eccc950a0d99ddd7cbcbd676
Lgvalia <le.gvalia@gmail.com>
2022-11-14 19:33:19 +0400

    Update app.py

--- app.py
+++ app.py
@@ -15,3 +15,10 @@ fig.show()
 fig.update_layout(title="Platform Count by Segment Types")
 # -- Input the Plotly chart to the Streamlit interface
 st.plotly_chart(fig, use_container_width=True)
+
+
+data['Percentage'] = data['Count'] / data.groupby(by=['Segment Type'])['Count'].transform('sum')
+fig_pct = px.bar(data, x='Segment Type',y='Percentage', color='Answer', text_auto=True, barmode='stack')
+fig_pct.layout.yaxis.tickformat = ',.2%'
+fig_pct.update_layout(title="Platform Percents by Segment Types")
+st.plotly_chart(fig_pct, use_container_width=True)
