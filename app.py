5a75d0f074c3e48fc15a107bba9eeb48c1612acf
Lgvalia <le.gvalia@gmail.com>
2022-11-14 20:34:17 +0400

    Update app.py

--- app.py
+++ app.py
@@ -4,8 +4,24 @@ import pandas as pd
 
 st.write('# Which Platform Users Click First')
 
-# -- Read in the data
+#Prepare Data
+#Main Df
 df = pd.read_csv('WhatsgoodlyData-10.csv')
+
+#Df for Custom Segment
+d1 = df.loc[df['Segment Type']=='Custom']
+d2 = d1.loc[d1['Segment Description'].str.split(' ').str.get(0)=='Graduation']
+d2['Segment Category'] = d2['Segment Description'].str.split(' ').str.get(0) + ' ' + d2['Segment Description'].str.split(' ').str.get(1)
+d2['Segment SubCategory'] = d2['Segment Description'].str.split(' ').str.get(-1)
+
+d3 = d1.loc[d1['Segment Description'].str.split(' ').str.get(0)!='Graduation']
+d3['Segment Category'] = d3['Segment Description'].str.split('?').str.get(0)
+d3['Segment SubCategory'] = d3['Segment Description'].str.split('?').str.get(1)
+
+df_custom = pd.concat([d1,d3])
+data_custom = df_custom.groupby(by=['Segment Category','Segment SubCategory','Answer'])['Count'].sum().reset_index()
+
+
 
 tab1, tab2, = st.tabs(["Summary", "Details"])
 
@@ -27,10 +43,15 @@ with tab1:
     st.plotly_chart(fig_pct, use_container_width=True)
 
 with tab2:
-   st.header("A dog")
-   st.image("https://static.streamlit.io/examples/dog.jpg", width=200)
-
-
-
-
-
+   st.write('Custom Segmet Type by Sub Categories')
+   category_choice = st.selectbox(
+        "Choose Category",
+        (['All'] + df_custom['Segment Category'].unique().tolist()),
+    ) 
+   if category_choice != "All":
+        filtered_df = data_custom[data_custom['Segment Category'] == category_choice]
+   else: filtered_df = data_custom
+   fig_custom = px.treemap(filtered_df, path=['Segment Category', 'Segment SubCategory', 'Answer'], values='Count')
+   fig_custom.data[0].textinfo = 'label+value'
+   #fig_custom.update_layout(title="Custom Segmet Type by Sub Categories")
+   st.plotly_chart(fig_custom, use_container_width=True)
